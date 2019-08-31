7dc0866853b8fa92dd9be403f0d530d144019e86
accetto <34798830+accetto@users.noreply.github.com>
2019-08-31 12:13:04 +0200

    release 19.08.1

--- src/firefox.plus/user.js
+++ src/firefox.plus/user.js
@@ -1,8 +1,16 @@
-// Issue: Firefox tab crashes "Gah. Your tab just crashed."
-// Mitigate the tab crashing problem by forcing the following preferences:
-user_pref("browser.tabs.remote.autostart", false);
-
 // Add the preferences you want to force here.
-// The preferences will be forced for each session, but only in the profile containing this file.
-// There is also a backup copy of this file in '/home/headless/firefox.plus'.
-// The VNC user ('headless:headless' by default) gets permissions to modify or delete this file.
+// They will be forced for each session, but only in profiles containing this file.
+
+// Disable WebRTC leaks as explained in https://ipleak.net/#webrtcleak
+// Be aware that this has impact on some applications, e.g. some messengers.
+// user_pref("media.peerconnection.enabled", false);
+
+// Fixing "Gah. Your tab just crashed."
+// Disabling Firefox multi-process through preferences doesn't work since version 68.0.
+// user_pref("browser.tabs.remote.autostart", false);
+// user_pref("browser.tabs.remote.autostart.2", false);
+// Disabling the Firefox multi-process must be done by setting the environment variable
+// MOZ_FORCE_DISABLE_E10S. Note that any value means 'yes'. Both following settings
+// will disable the multi-process feature:
+//      MOZ_FORCE_DISABLE_E10S=1
+//      MOZ_FORCE_DISABLE_E10S=0
